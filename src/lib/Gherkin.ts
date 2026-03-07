ea6c2a051dc12a4219bb6bdde7bca165b545e431
Baptiste <you@example.com>
2026-03-07 12:39:56 +0100

    Refactor formatGherkinLines

--- src/lib/Gherkin.ts
+++ src/lib/Gherkin.ts
@@ -1,16 +1,14 @@
 
-export type gherkinKeywords = "Feature" | "Background" | "Rule" | "Scenario" | "Scenario Outline" | "Examples" | "Given" | "When" | "Then" | "And" | "But"
-
-export const gherkinEnglishKeywords = new Map<gherkinKeywords, string[]>()
-gherkinEnglishKeywords.set("Feature", ["Feature", "Business", "NeedAbility"])
-gherkinEnglishKeywords.set("Background", ["Background"])
-gherkinEnglishKeywords.set("Rule", ["Rule"])
-gherkinEnglishKeywords.set("Scenario", ["Scenario", "Example"])
-gherkinEnglishKeywords.set("Scenario Outline", ["Scenario Outline", "Scenario Template"])
-gherkinEnglishKeywords.set("Examples", ["Examples", "Scenarios"])
-gherkinEnglishKeywords.set("Given", ["Given", "*"])
-gherkinEnglishKeywords.set("When", ["When", "*"])
-gherkinEnglishKeywords.set("Then", ["Then", "*"])
-gherkinEnglishKeywords.set("And", ["And", "*"])
-gherkinEnglishKeywords.set("But", ["But", "*"])
-
+export const gherkinKeywords = {
+    Feature: ["Feature", "Business", "NeedAbility"],
+    Background: ["Background"],
+    Rule: ["Rule"],
+    Scenario: ["Scenario", "Example"],
+    ScenarioOutline: ["Scenario Outline", "Scenario Template"],
+    Examples: ["Examples", "Scenarios"],
+    Given: ["Given", "*"],
+    When: ["When", "*"],
+    Then: ["Then", "*"],
+    And: ["And", "*"],
+    But: ["But", "*"],
+}
